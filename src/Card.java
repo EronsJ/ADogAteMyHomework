9d9cba2a321354421e251e6605f49ed52bb1c120
Guest <kil4911@g.rit.edu>
2016-07-11 00:04:25 -0500

    ok

--- src/Card.java
+++ src/Card.java
@@ -8,15 +8,33 @@
     public class Card{
         int number;
         CardSuite shape;
+        ActionCardType actionType;
 
         public Card(int number , CardSuite shape){
             this.number=number;
             this.shape=shape;
         }
 
+        public void setActionType(ActionCardType actionType) {
+            this.actionType = actionType;
+        }
+
+
+        public int getNumber() {
+            return number;
+        }
+
+        public CardSuite getShape() {
+            return shape;
+        }
+
+        public ActionCardType getAction() {
+            return actionType;
+        }
+
         @Override
         public String toString() {
-            return ("("+this.number+this.shape+")");
+            return ("("+this.number+this.shape+this.actionType+")");
         }
     }
 
